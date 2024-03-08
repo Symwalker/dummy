68b27ff29debc681848ccc5c627ef248e9cb150c
Symwalker <shayanhanif50@gmail.com>
2024-03-08 09:21:30 +0500

    finalising the projects

---
+++ app/meme/[id]/page.js
@@ -0,0 +1,15 @@
+import Meme from "./meme"
+
+export default async function page({params}){
+    const {id} = params
+    let memes =await fetch('https://api.imgflip.com/get_memes')
+    memes = await memes.json()
+    memes = memes.data.memes
+    let singleMeme = memes.filter((meme)=>meme.id === id) 
+    singleMeme = singleMeme[0] 
+    console.log(singleMeme);
+    return<div>
+        <h2 className="text-white underline mb-10 font-semibold text-center mt-5 text-[30px]">{singleMeme.name}</h2>
+        <Meme singleMeme={singleMeme}/>
+    </div>
+}
--- app/page.js
+++ app/page.js
@@ -2,46 +2,30 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default async function Home() {
-  let memes =await fetch('https://api.imgflip.com/get_memes')
-  memes = await memes.json()
-  memes = memes.data.memes
-  // console.log(memes);
+  let memes = await fetch("https://api.imgflip.com/get_memes");
+  memes = await memes.json();
+  memes = memes.data.memes;
   return (
-  //  <div className="grid grid-cols-4 w-[80%] mx-auto">
-  //     {
-  //       memes?.map((meme)=>(
-  //         <Link href={`/meme/${meme.id}`}>
-  //         <div className= {`col-span-1 w-full  border-2  grayscale hover:grayscale-0 duration-500 cursor-pointer border-black `} >
-  //           <h2 className="text-center">{meme.name}</h2>
-  //           <img src={meme.url} className="bg-contain "  />
+    <div>
+      <header className="fixed  left-0 right-0    top-10 z-30 mx-auto  w-[80%] border border-gray-100 bg-white/80 py-3 shadow backdrop-blur-lg rounded-3xl ">
+        <div className=" text-center text-[30px] font-bold">MEME GENERATOR</div>
+      </header>
 
-  //         </div>
-  //         </Link>
-  //       ))
-  //     }
-  //  </div>
-  <div className=" ">
-<header className="fixed  left-0 right-0    top-10 z-30 mx-auto  w-[80%] border border-gray-100 bg-white/80 py-3 shadow backdrop-blur-lg rounded-3xl ">
-
-      <div className=" text-center text-[30px] font-bold">
-        MEME GENERATOR
+      <div className="masonry-gallery w-[80%] mx-auto mt-36">
+        {memes?.map((meme) => (
+          <div
+            key={meme.id}
+            className="masonry-item w-full rounded-md  bg-white   "
+          >
+            <Link href={`/meme/${meme.id}`}>
+              <div className="  grayscale hover:grayscale-0 duration-500 cursor-pointer">
+                <h2 className="text-center text-black">{meme.name}</h2>
+                <img src={meme.url} alt={meme.name} className="w-full" />
+              </div>
+            </Link>
+          </div>
+        ))}
       </div>
-      
-</header>
-
-  <div className="masonry-gallery w-[80%] mx-auto mt-36">
-  {memes?.map((meme) => (
-    <div key={meme.id} className="masonry-item w-full rounded-md  bg-white   ">
-      <Link href={`/meme/${meme.id}`}>
-        <div className="  grayscale hover:grayscale-0 duration-500 cursor-pointer">
-          <h2 className="text-center text-black">{meme.name}</h2>
-          <img src={meme.url} alt={meme.name} className="w-full" />
-        </div>
-      </Link>
     </div>
-  ))}
-</div>
-</div>
-
   );
 }
